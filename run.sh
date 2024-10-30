0b7438f2e0a52ad682414adafa7eb4de39404a07
minhng92 <minhng92@gmail.com>
2024-10-30 13:06:24 +0700

    update permissions

--- run.sh
+++ run.sh
@@ -2,18 +2,35 @@
 DESTINATION=$1
 PORT=$2
 CHAT=$3
-# clone Odoo directory
+
+# Clone Odoo directory
 git clone --depth=1 https://github.com/minhng92/odoo-18-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
-# set permission
+
+# Create PostgreSQL directory
 mkdir -p $DESTINATION/postgresql
-sudo chmod -R 777 $DESTINATION
-# config
-if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
+
+# Change ownership to current user and set restrictive permissions for security
+sudo chown -R $USER:$USER $DESTINATION
+sudo chmod -R 700 $DESTINATION  # Only the user has access
+
+# System configuration
+if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then 
+    echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf)
+else 
+    echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf
+fi
 sudo sysctl -p
+
+# Set ports in docker-compose.yml
 sed -i 's/10018/'$PORT'/g' $DESTINATION/docker-compose.yml
 sed -i 's/20018/'$CHAT'/g' $DESTINATION/docker-compose.yml
-# run Odoo
+
+# Set file and directory permissions after installation
+find $DESTINATION -type f -exec chmod 644 {} \;
+find $DESTINATION -type d -exec chmod 755 {} \;
+
+# Run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: minhng.info | Live chat port: '$CHAT
+echo "Odoo started at http://localhost:$PORT | Master Password: minhng.info | Live chat port: $CHAT"
