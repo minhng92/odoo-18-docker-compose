bfab5851d75f1f419c116743fa8dacfaaf1a0565
Andrei Boyanov <andrei.boyanov@gmail.com>
2025-04-19 10:56:12 +0200

    Update run.sh to use docker compose instead of docker-compose
    Signed-off-by: Andrei Boyanov <andrei.boyanov@gmail.com>

--- run.sh
+++ run.sh
@@ -46,6 +46,6 @@ find $DESTINATION -type d -exec chmod 755 {} \;
 chmod +x $DESTINATION/entrypoint.sh
 
 # Run Odoo
-docker-compose -f $DESTINATION/docker-compose.yml up -d
+docker compose -f $DESTINATION/docker-compose.yml up -d
 
 echo "Odoo started at http://localhost:$PORT | Master Password: minhng.info | Live chat port: $CHAT"
